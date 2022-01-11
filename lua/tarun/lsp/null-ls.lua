42a528468838e715b00313000d3546ecdf3f1001
TarunDaCoder <tarunsnair09@gmail.com>
2022-01-11 10:05:17 +0530

    add a format-on-save snippet

--- lua/tarun/lsp/null-ls.lua
+++ lua/tarun/lsp/null-ls.lua
@@ -19,4 +19,14 @@ null_ls.setup {
     formatting.stylua,
     diagnostics.luacheck,
   },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
+        augroup LspFormatting
+          autocmd! *  <buffer>
+          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+        augroup END
+      ]])
+    end
+  end
 }
