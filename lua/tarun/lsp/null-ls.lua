b4898f35070581272db6e30f5bf4e57cb20e4537
TarunDaCoder <tarunsnair09@gmail.com>
2022-01-11 12:24:13 +0530

    try and fix the formatting thing, code from @vsvedov, I think I spelled that right

--- lua/tarun/lsp/null-ls.lua
+++ lua/tarun/lsp/null-ls.lua
@@ -21,12 +21,7 @@ null_ls.setup {
   },
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
-      vim.cmd([[
-        augroup LspFormatting
-          autocmd! *  <buffer>
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-        augroup END
-      ]])
+      vim.cmd[[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
     end
   end
 }
