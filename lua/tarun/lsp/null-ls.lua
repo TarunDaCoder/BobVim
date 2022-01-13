88973d00f41419c10ae694eb4fb8de0b961b0b9a
TarunDaCoder <tarunsnair09@gmail.com>
2022-01-13 10:38:18 +0530

    change autostart to true and add python linting and formatting

--- lua/tarun/lsp/null-ls.lua
+++ lua/tarun/lsp/null-ls.lua
@@ -18,10 +18,14 @@ null_ls.setup {
     -- Lua
     formatting.stylua,
     diagnostics.luacheck,
+    -- Python
+    diagnostics.flake8,
+    formatting.black,
   },
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
       vim.cmd[[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
     end
-  end
+  end,
+  autostart = true,
 }
