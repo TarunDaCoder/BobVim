a48f43a253e3d151b89e0ed52479d881d5b8fb81
TarunDaCoder <tarunsnair09@gmail.com>
2022-01-14 09:55:15 +0530

    fix license year

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@
 -- |____/ \__,_|_| \_| \_/ |_|_| |_| |_|
 -- Author: https://github.com/TarunDaCoder
 -- GitHub: https://github.com/TarunDaCoder/DaNvim
--- License: MIT License Copyright (c) 20221 by TarunDaCoder
+-- License: MIT License Copyright (c) 2022 by TarunDaCoder
 
 vim.opt.termguicolors = true -- Set termguicolors
 
