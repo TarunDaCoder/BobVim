1709ac7c8156d67da2634e599f0db1298c29cf52
TarunDaCoder <tarunsnair09@gmail.com>
2022-02-01 19:28:27 +0530

    delete old lsp config

--- lua/tarun/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
-require('tarun.lsp.lsp-installer')
-require('tarun.lsp.handlers').setup()
--- require('tarun.lsp.null-ls')
