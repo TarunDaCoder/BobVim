e152309d0f036653293f0937a61c3b52babaa391
TarunDaCoder <tarunsnair09@gmail.com>
2022-02-03 18:13:15 +0530

    add a custom telescope theme

--- lua/tarun/utils.lua
+++ lua/tarun/utils.lua
@@ -1,1 +1,16 @@
 local Util = {}
+
+Util.telescope_theme = {
+    results_title = false,
+    layout_strategy = "center",
+    previewer = false,
+    winblend = 30,
+    layout_config = { width = 0.6, height = 0.6 },
+    borderchars = {
+        preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        prompt = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        results = { "─", " ", " ", " ", "╰", "╯", " ", " " },
+    },
+}
+
+return Util
