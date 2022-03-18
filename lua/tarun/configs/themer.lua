58a7df1b1a0fd71326d3fd175ce47bd3c7cd28bd
TarunDaCoder <tarunsnair09@gmail.com>
2022-03-18 11:25:23 +0530

    format themer

--- lua/tarun/configs/themer.lua
+++ lua/tarun/configs/themer.lua
@@ -1,11 +1,11 @@
-require("themer").setup({
-	colorscheme = "onedark",
+require('themer').setup({
+	colorscheme = 'dracula',
 	styles = {
-		comment = { style = "italic" },
-		["function"] = { style = "italic" },
-		functionbuiltin = { style = "italic" },
-		variable = { style = "italic" },
-		variableBuiltIn = { style = "italic" },
-		parameter = { style = "italic" },
+		comment = { style = 'italic' },
+		['function'] = { style = 'italic' },
+		functionbuiltin = { style = 'italic' },
+		variable = { style = 'italic' },
+		variableBuiltIn = { style = 'italic' },
+		parameter = { style = 'italic' },
 	},
 })
