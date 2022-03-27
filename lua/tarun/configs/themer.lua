a02b0cf2efcbf260431a7340a2255287a7b370b4
TarunDaCoder <tarunsnair09@gmail.com>
2022-03-27 09:19:11 +0530

    use catppuccin storm

--- lua/tarun/configs/themer.lua
+++ lua/tarun/configs/themer.lua
@@ -1,11 +1,14 @@
-require('themer').setup({
-	colorscheme = 'dracula',
-	styles = {
-		comment = { style = 'italic' },
-		['function'] = { style = 'italic' },
-		functionbuiltin = { style = 'italic' },
-		variable = { style = 'italic' },
-		variableBuiltIn = { style = 'italic' },
-		parameter = { style = 'italic' },
-	},
-})
+-- require('themer').setup({
+-- colorscheme = 'catppuccin',
+-- styles = {
+-- comment = { style = 'italic' },
+-- ['function'] = { style = 'italic' },
+-- functionbuiltin = { style = 'italic' },
+-- variable = { style = 'italic' },
+-- variableBuiltIn = { style = 'italic' },
+-- parameter = { style = 'italic' },
+-- },
+-- })
+
+vim.g.catppuccin_flavour = 'storm'
+vim.cmd([[colorscheme catppuccin]])
