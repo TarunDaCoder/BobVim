9d64ae7f99aa4bfc4a04393ce3b03541c0b11426
TarunDaCoder <tarunsnair09@gmail.com>
2022-04-15 12:21:46 +0530

    fix nvimtree

--- lua/tarun/configs/nvim_tree.lua
+++ lua/tarun/configs/nvim_tree.lua
@@ -3,7 +3,6 @@ local g = vim.g
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 g.nvim_tree_git_hl = 0
 g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_indent_markers = 1
 g.nvim_tree_root_folder_modifier = table.concat({ ':t:gs?$?/..', string.rep(' ', 1000), '?:gs?^??' })
 
 g.nvim_tree_show_icons = {
@@ -112,4 +111,9 @@ nvim_tree.setup({
 			},
 		},
 	},
+    renderer = {
+        indent_markers = {
+            enable = true,
+        },
+    },
 })
