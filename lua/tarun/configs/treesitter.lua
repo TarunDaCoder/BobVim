c32c01ab23ab1a5034727231a403c1895cbeaa7c
TarunDaCoder <tarunsnair09@gmail.com>
2022-04-24 10:45:48 +0530

    im not using dracula anymore so...

--- lua/tarun/configs/treesitter.lua
+++ lua/tarun/configs/treesitter.lua
@@ -47,11 +47,5 @@ require('nvim-treesitter.configs').setup({
 		enable = true,
 		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
 		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-		colors = {
-			'#ff5555',
-			'#8be9fd',
-			'#50fa7b',
-			'#ff79c6',
-		},
 	},
 })
