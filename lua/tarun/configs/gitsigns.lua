69061461cc6371571148bc637f2b52cc0696367b
TarunDaCoder <tarunsnair09@gmail.com>
2022-04-24 10:45:13 +0530

    idk

--- lua/tarun/configs/gitsigns.lua
+++ lua/tarun/configs/gitsigns.lua
@@ -4,9 +4,11 @@ if not status_ok then
 end
 
 gitsigns.setup({
+  signcolumn = true,
+  numhl = true,
 	current_line_blame = true,
-	preview_config = {
-		border = 'single',
-		style = 'minimal',
-	},
+	-- preview_config = {
+	-- 	border = 'single',
+	-- 	style = 'minimal',
+	-- },
 })
