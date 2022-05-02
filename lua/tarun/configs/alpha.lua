94ee8aaefc5b0303015d5b6b34f81d4d539b66fa
TarunDaCoder <tarunsnair09@gmail.com>
2022-05-02 09:39:29 +0530

    fix(ui):fix the alpha errors when using sessions of projects

--- lua/tarun/configs/alpha.lua
+++ lua/tarun/configs/alpha.lua
@@ -19,12 +19,12 @@ dashboard.section.buttons.val = {
   dashboard.button(
     "p",
     "" .. " Find project",
-    ":lua require('telescope').extensions.projects.projects()<CR>"
+    ":Telescope project project <CR>"
   ),
   dashboard.button("r", "" .. " Recent files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "" .. " Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("s", "" .. " Find Session", ":Telescope sessions save_current=false <CR>"),
-  dashboard.button("c", "" .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("s", "" .. " Find Session", ":Telescope sessions_picker <CR>"),
+  dashboard.button("c", "" .. " Config", ":e ~/.config/bv/init.lua <CR>"),
   dashboard.button("q", "" .. " Quit", ":qa<CR>"),
 }
 
