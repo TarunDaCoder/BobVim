848b830d5a52398c66ba6d7f0d25dc92929b1076
TarunDaCoder <tarunsnair09@gmail.com>
2022-05-25 11:37:49 +0530

    change this cuz I stopped using cheovim

--- lua/tarun/configs/alpha.lua
+++ lua/tarun/configs/alpha.lua
@@ -20,7 +20,7 @@ dashboard.section.buttons.val = {
 	dashboard.button('r', '' .. ' Recent files', ':Telescope oldfiles <CR>'),
 	dashboard.button('t', '' .. ' Find text', ':Telescope live_grep <CR>'),
 	dashboard.button('s', '' .. ' Find Session', ':Telescope sessions_picker <CR>'),
-	dashboard.button('c', '' .. ' Config', ':e ~/.config/bv/init.lua <CR>'),
+	dashboard.button('c', '' .. ' Config', ':e ~/.config/nvim/init.lua <CR>'),
 	dashboard.button('q', '' .. ' Quit', ':qa<CR>'),
 }
 
